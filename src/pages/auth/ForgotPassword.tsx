bff922819834efa4a9a5d27ea9df9c10f88cbbe5
irpanzy <irfanoputra27@gmail.com>
2026-06-13 23:56:47 +0700

    feat(pages): add authentication page components

---
+++ src/pages/auth/ForgotPassword.tsx
@@ -0,0 +1,5 @@
+import { ForgotPasswordFeature } from '@/features/auth'
+
+export default function ForgotPassword() {
+  return <ForgotPasswordFeature />
+}
---
+++ src/pages/auth/ResetPassword.tsx
@@ -0,0 +1,5 @@
+import { ResetPasswordFeature } from '@/features/auth'
+
+export default function ResetPassword() {
+  return <ResetPasswordFeature />
+}
